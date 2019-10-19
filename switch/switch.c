63d0a41adc618ca0ea0f0ea382e3e972e452f2fb
svscsantosh <54067868+svscsantosh@users.noreply.github.com>
2019-10-20 02:07:25 +0530

    Update Switch
    Used Python For Program

--- switch/switch.c
+++ switch/switch.c
@@ -1,55 +1,10 @@
-#include<conio.h>
-#include <stdio.h>
-#include<stdio.h>
-
-
-int main()
-{
-    int num,ch;
-    printf("enter the  two digit number of your choice");
-    scanf("%d",&num);
-    while(ch!=2){
-    printf("This is the following menu options\n");
-    printf("----------1.Choose here for incrementing the number-----------------\n");
-    printf("----------2.Choose here for decrementing the number-------------------\n");
-    printf("----------3.EXIT THE PROGRAM----------------------------\n");
-    printf("enter the choice");
-    scanf("%d",&ch);
-    
-    switch(num) {
-        case 1: {
-            int res;
-            res=incr(num);
-            printf("the incremented number is %d\n",res);
-            break;
-        }
-        case 2:{
-            int res;
-            res=decr(num);
-            printf("the decremented number is %d\n",res);
-            break;
-            
-        }
-        case 3:{
-            exit(0);
-            break;
-        }
-    }
-    }       
-    
-
-    return 0;
-}
-
-int incr(int x)
-{
-     x=x+1;
-     return x;
-}
-int decr(int x)
-{
-    x=x-1;
-    return x;
-}
-
-
+n=int(input("Enter The number"))
+choice=int(input("Enter the choice"))
+if(choice==1):
+    n=n+1
+    print(n)
+elif(choice==2):
+    n=n-1
+    print(n)
+elif(choice==3):
+    pass
