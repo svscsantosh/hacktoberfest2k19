2dfbf3b1258b8da18b965826dd077df8967c38da
angadpal31 <48057370+angadpal31@users.noreply.github.com>
2019-10-20 00:56:10 +0530

    Update switch.c

--- switch/switch.c
+++ switch/switch.c
@@ -1,22 +1,23 @@
 #include<conio.h>
-#include<iostream.h
+#include <stdio.h>
 #include<stdio.h>
-
+int incr(int);
+int decr(int);
 
 int main()
 {
     int num,ch;
-    printf("enter the  two digit number of your choice\n");
+    printf("enter the  two digit number of your choice");
     scanf("%d",&num);
-    while(ch!=3){ //replaced ch = 2 with ch = 3
+    do{
     printf("This is the following menu options\n");
     printf("----------1.Choose here for incrementing the number-----------------\n");
     printf("----------2.Choose here for decrementing the number-------------------\n");
     printf("----------3.EXIT THE PROGRAM----------------------------\n");
     printf("enter the choice");
     scanf("%d",&ch);
-
-    switch(ch) { // replaced num with ch
+    
+    switch(ch) {
         case 1: {
             int res;
             res=incr(num);
@@ -26,23 +27,17 @@ int main()
         case 2:{
             int res;
             res=decr(num);
-            res=decr(res);//decremented twice 
             printf("the decremented number is %d\n",res);
             break;
-
+            
         }
         case 3:{
-            exit(0);
-            break;
+            return 0;
         }
-
     }
-    }
-
-
+    }while(ch!=3);
     return 0;
-}
-
+    }       
 int incr(int x)
 {
      x=x+1;
