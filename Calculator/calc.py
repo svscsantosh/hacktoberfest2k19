25651d31a6279d212835d034b919e90cf590d4c9
mishikaraj <49092742+mishikaraj@users.noreply.github.com>
2019-10-20 02:14:57 +0530

    Update calc.py

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -89,6 +89,7 @@ butdiv.place(x=205,y=310)
 butclear=Button(me,padx=14,pady=119,bd=4,bg='white',text="CE",command=clrbut,font=("Courier New",16,'bold'))
 butclear.place(x=270,y=100)
 
+
 butequal=Button(me,padx=151,pady=14,bd=4,bg='white',command=equlbut,text="=",font=("Courier New",16,'bold'))
 butequal.place(x=10,y=380)
 me.mainloop()
