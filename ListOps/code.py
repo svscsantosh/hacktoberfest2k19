94c598bdd5ae3b40ab6cf39877392d1a798e216c
svscsantosh <54067868+svscsantosh@users.noreply.github.com>
2019-10-20 03:15:20 +0530

    List appeal

--- ListOps/code.py
+++ ListOps/code.py
@@ -1,22 +1,7 @@
-'''
-Task: Delete alternate elements of the given list, starting from index 0,
-      print the list after each traversal.
-
-Example: given list -> [1,2,3,4,5]
-                o/p: [2, 4]
-                     [4]
-                     []
-
-'''
-
-nums = [3, 5, 2, 9, 101, 333, 4]
-
-def remove_odd_element(input_list):
-    i=0
-    for element in input_list:
-        if i % 2 == 0:
-            pass
-        else:
-            input_list.remove(element)
-        i = i + 1
-
+num=[1,2,3,4,5]
+i=0
+while i in range(len(num)+1):
+    del num[0::2]
+    print(num)
+    i=i+1
+print("[]")
