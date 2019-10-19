9d8d73015425ee7c0fde79a5e5553f997f34829f
svscsantosh <54067868+svscsantosh@users.noreply.github.com>
2019-10-20 02:30:56 +0530

    Update primefactorization.py

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,14 +1,4 @@
-import math 
-def is_prime(n): 
-    if n <= 1: 
-        return False
-    max_div = math.floor(math.sqrt(n)) 
-    for i in range(2, 1 + max_div): 
-        if n % i == 0: 
-            return False
-    return True
-k = int(input("Enter a number:"))
-print("Prime numbers are:")
-for n in range(1,k): 
-    x = is_prime(n) 
-    print(x,end = '')
+import sympy
+n=int(input("Enter an integer:"))
+for i in range(n):
+    print(sympy.isprime(n),end="")
